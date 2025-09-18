4143e02a1c55c88e94d9f7ad570033b2ff452179
Alexander Liu <alexander_l_22@hotmail.com>
2025-09-17 23:24:27 -0400

    [a1][deadlock] add deadlock example program

---
+++ a1/DeadlockExample.java
@@ -0,0 +1,55 @@
+public class DeadlockExample {
+
+    private static class DeadlockExampleThread1 extends Thread {
+
+        public void run() {
+            synchronized (lock1) {
+                System.out.println("Thread 1 holding lock 1...");
+
+                try {
+                    Thread.sleep(1000); // sleep to increase chances of deadlock
+                } catch (InterruptedException e) {
+                    // ignore exception
+                }
+
+                System.out.println("Thread 1 waiting for lock 2...");
+
+                synchronized (lock2) {
+                    System.out.println("Thread 1 holding locks 1 and 2...");
+                }
+            }
+        }
+    }
+
+    private static class DeadlockExampleThread2 extends Thread {
+
+        public void run() {
+            synchronized (lock2) {
+                System.out.println("Thread 2 holding lock 2...");
+
+                try {
+                    Thread.sleep(1000); // sleep to increase chances of deadlock
+                } catch (InterruptedException e) {
+                    // ignore exception
+                }
+
+                System.out.println("Thread 2 waiting for lock 1...");
+
+                synchronized (lock1) {
+                    System.out.println("Thread 2 holding locks 1 and 2...");
+                }
+            }
+        }
+    }
+
+    public static Object lock1 = new Object();
+    public static Object lock2 = new Object();
+
+    public static void main(String[] args) {
+        DeadlockExampleThread1 t1 = new DeadlockExampleThread1();
+        DeadlockExampleThread2 t2 = new DeadlockExampleThread2();
+
+        t1.start();
+        t2.start();
+    }
+}
