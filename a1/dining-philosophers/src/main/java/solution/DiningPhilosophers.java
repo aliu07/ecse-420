eb40e44456f678574896ea7c28b0815ef7b6e515
Alexander Liu <alexander_l_22@hotmail.com>
2025-09-24 20:58:38 -0400

    [a1][dphils] suppress unused warnings & add todos

--- a1/dining-philosophers/src/main/java/solution/DiningPhilosophers.java
+++ a1/dining-philosophers/src/main/java/solution/DiningPhilosophers.java
@@ -1,14 +1,17 @@
 package solution;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.locks.ReentrantLock;
+// TODO: implement ts
+// import java.util.concurrent.ExecutorService;
+// import java.util.concurrent.Executors;
+// import java.util.concurrent.locks.ReentrantLock;
 
 public class DiningPhilosophers {
 
     public static void main(String[] args) {
         int numberOfPhilosophers = 5;
+        @SuppressWarnings("unused") // TODO: remove
         Philosopher[] philosophers = new Philosopher[numberOfPhilosophers];
+        @SuppressWarnings("unused") // TODO: remove
         Object[] chopsticks = new Object[numberOfPhilosophers];
     }
 
